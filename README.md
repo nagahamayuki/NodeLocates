e2d9fc213815d25633a92e65f2c1d11298139e6e
nagahamayuki <nagahamayuki@newrevo.jp>
2016-11-30 11:07:40 +0900

    add app.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NodeLocates
-# NodeLocates
+
+■node製のサーバー
+
+サーバーを立ち上げて、socket.ioにて接続。
+node_moduleにてsocket.ioが必要です。
---
+++ app.js
@@ -0,0 +1,36 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer();
+server.on("request", function(req, res){
+  fs.readFile("./index.html", "UTF-8", function(err, data){
+    res.setHeader("Content-Type", "text/html");
+    res.write(data);
+    res.end();
+  });
+}).listen(1234);
+
+
+var io = require("socket.io")(server);
+io.on("connection", function(client){
+
+  console.log("サーバーに接続されました。");
+
+  client.on('disconnect', function(){
+    console.log('接続を解除しました。');
+  });
+
+  client.on("publish", function(data){
+    console.log(data);
+    io.emit("publishMap", {lat: data.lat, lng: data.lng});
+  });
+
+  client.on("startSocket", function(e){
+    console.log(e);
+  })
+
+  client.on("stopSocket", function(e){
+    console.log(e);
+  })
+
+});
